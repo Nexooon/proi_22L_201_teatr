460ec884e5abd386ff7aa6499e95977a28ed2290
kkulak1 <01169200@pw.edu.pl>
2022-05-19 21:58:17 +0200

    date

--- data.h
+++ data.h
@@ -24,7 +24,6 @@ public:
     Data(const Data *data);
     virtual ~Data();
 
-    // void read(string path);
     void read_plays(string plays_path);
     void read_halls(string halls_path);
 
@@ -33,16 +32,6 @@ public:
     int comedies_size();
     int dramas_size();
     int tragedies_size();
-
-    // friend void add_play_obj(vector<Play> plays, string title, string price, string time);
-    // friend void add_hall_obj(vector<Hall> halls, string hnr, string hseats);
-    // friend int get_header(string line);
 };
-
-// void add_play_obj(vector<Play> plays, string title, string price, string time);
-// void add_hall_obj(vector<Hall> halls, string hnr, string hseats);
-// int get_header(string line);
-
-// Hall read_hall_obj(string line, int nline);
 
 #endif
